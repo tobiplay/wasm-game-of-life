b3b50951966b3a45f62ab820257e54715cda3f12
tobiplay <tobiplay@fa-meier.de>
2022-08-07 17:24:08 +0200

    Add folders from dir structure

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn test() {
+    alert("This is a rusty test!");
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hellooooo, wasm-game-of-life!");
+}
