ad2a6dcfeaaacff291c6d6f0a32034a6f0c852f6
tobiplay <tobiplay@fa-meier.de>
2022-08-07 18:30:45 +0200

    Add method for counting living neighbors of a cell and determining next state based on rules

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,8 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
+// These functions won't be exposed to JavaScript as
+// they're only meant to handle the internal state:
 impl Universe {
     /// Returns the index of a `Cell` within an `Universe`.
     ///
@@ -49,7 +51,7 @@ impl Universe {
     /// We multiply the `row` with the actual `width` of an `Universe` and add
     /// the `column` to the value.
     ///
-    /// # Explanation of algorithm
+    /// # Algorithm explanation
     ///
     /// Let's examine the return value for an example
     /// array. For this we'll assume an `Universe`
@@ -67,10 +69,96 @@ impl Universe {
     /// we're looking for (starting at 0).
     ///
     /// # Panics
-    /// The method panics if at least one of `row` and 
+    /// The method panics if at least one of `row` and
     /// `column` is negative.
     fn get_index(&self, row: u32, column: u32) -> usize {
         assert!(row > 0 && column > 0);
         (row * self.width + column) as usize
+    }
+
+    /// Counts the living neighbours of a cell.
+    ///
+    /// We provide the `row` and `column` of the `Cell`
+    /// for which we'd like to get the number of living
+    /// neighbors returned.
+    ///
+    /// # Algorithm explanation
+    ///
+    /// The method uses deltas and modulo (`%`) to avoid special
+    /// casing the edges of an `Universe` with `if` statements.
+    /// When applying a delta of -1, we add `self.height`
+    /// minus 1 and let the modulo do its thing, rather
+    /// than attempting to subtract 1 directly. Both `row`
+    /// and `column` can be 0, and if we attempted to subtract 1
+    /// from them, there would be an unsigned integer underflow.
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        // Keep track of the number of living neighbors:
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                // When this is true, we're currently looking at the
+                // center cell and not its neighbors, so we just continue.
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                // Calculate both the row and column of
+                // the neighboring cell:
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                // Get the index of the neighboring cell:
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
+
+// These methods for Universe will be exposed to the
+// JavaScript API:
+#[wasm_bindgen]
+impl Universe {
+    /// Advances the time t one tick in time (= delta).
+    pub fn tick(&mut self) {
+        // Clone the current cells into a new vector:
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                // Get the index of the current cell:
+                let idx = self.get_index(row, col);
+                // Get the cell at the specific index:
+                let cell = self.cells[idx];
+                // Count the number of living neighbors:
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                // Determine the state of the cell in the next tick in time:
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                // Insert the next_cell into the array of
+                // cells at the next tick in time:
+                next[idx] = next_cell;
+            }
+        }
+
+        // Overwrite the current array of cells with
+        // those at the current time plus one tick:
+        self.cells = next;
     }
 }
