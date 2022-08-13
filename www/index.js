1da64aef4818347eda9606c6beb58d94140b6537
tobiplay <tobiplay@fa-meier.de>
2022-08-13 16:52:49 +0200

    Add dummy button

--- www/index.js
+++ www/index.js
@@ -136,10 +136,10 @@ drawCells();
 drawGrid();
 requestAnimationFrame(renderLoop);
 
-// var button = document.createElement("button");
-// button.innerHTML = "Do Something";
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
 
-// var body = document.getElementsByTagName("body")[0];
-// body.appendChild(button);
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
 
-// button.addEventListener("click", () => {});
+button.addEventListener("click", () => {});
