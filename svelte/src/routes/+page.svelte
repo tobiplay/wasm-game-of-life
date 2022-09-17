c43fc8d77090281b528816c7b189e24eb39dfb38
tobiplay <tobiplay@fa-meier.de>
2022-09-17 11:50:51 +0200

    Extend Vite serving allow list with `../rust/pkg`

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-    alert(1)
+    import { onMount } from 'svelte';
+    import init, {greet} from "wasm-game-of-life";
+        
+    onMount(async () => {
+        init();
+    })
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => greet()}>Click</button>
