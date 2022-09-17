b95275ccb244c29cbcb787791cdb60615ba8935f
tobiplay <tobiplay@fa-meier.de>
2022-09-17 11:50:51 +0200

    Redesign `live_neighbor_count` method
    The method turned out to have a major impact on the overall app
    performance, due to the utilization of the modulo operation. To improve
    performance, the method was redesigned to rely on a `if` control flow
    pattern to count the living neighbors of a given cell and handle the
    special cases at the edge of the universe.

---
+++ benches/bench.rs
@@ -0,0 +1,13 @@
+// #![feature(test)]
+
+// extern crate test;
+// extern crate wasm_game_of_life;
+
+// #[bench]
+// fn universe_ticks(b: &mut test::Bencher) {
+//     let mut universe = wasm_game_of_life::Universe::new(wasm_game_of_life::UniverseOption::Random);
+
+//     b.iter(|| {
+//         universe.tick();
+//     });
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -132,26 +132,101 @@ impl Universe {
     /// than attempting to subtract 1 directly. Both `row`
     /// and `column` can be 0, and if we attempted to subtract 1
     /// from them, there would be an unsigned integer underflow.
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        // Keep track of the number of living neighbors:
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                // When this is true, we're currently looking at the
-                // center cell and not its neighbors, so we just continue.
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
+    // fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+    //     // Keep track of the number of living neighbors:
+    //     let mut count = 0;
+    //     for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+    //         for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+    //             // When this is true, we're currently looking at the
+    //             // center cell and not its neighbors, so we just continue.
+    //             if delta_row == 0 && delta_col == 0 {
+    //                 continue;
+    //             }
 
-                // Calculate both the row and column of
-                // the neighboring cell:
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                // Get the index of the neighboring cell:
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
+    //             // Calculate both the row and column of
+    //             // the neighboring cell:
+    //             let neighbor_row = (row + delta_row) % self.height;
+    //             let neighbor_col = (column + delta_col) % self.width;
+    //             // Get the index of the neighboring cell:
+    //             let idx = self.get_index(neighbor_row, neighbor_col);
+    //             count += self.cells[idx] as u8;
+    //         }
+    //     }
+    //     count
+    // }
+
+    /// Counts the living neighbours of a cell.
+    ///
+    /// We provide the `row` and `column` of the a cell
+    /// for which we'd like to get the number of living
+    /// neighbors (cells) returned.
+    ///
+    /// # Algorithm explanation
+    ///
+    /// First, we find the north, south, west and east cell to
+    /// our current cell that we've defined via `row` and `column`.
+    ///
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        // Determine the north, west, east and south cells:
+        // If we're at the top row, we wrap to the last row by
+        // subtracting 1 from the height of the universe. Note that
+        // in Rust the index starts at 0, which is why we have to
+        // substract 1. Otherwise we just return the row - 1.
+        let north = if row == 0 { self.height - 1 } else { row - 1 };
+
+        // If we're at the bottom row, we wrap to the first row by
+        // returning a 0. Otherwise the row below is just the row + 1.
+        let south = if row == self.height - 1 { 0 } else { row + 1 };
+
+        // If we're at the leftmost column, we wrap to the rightmost
+        // column by returning the width of the universe - 1. Otherwise
+        // the column to the left is just the column - 1.
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+
+        // If we're at the rightmost column, we wrap to the leftmost
+        // column by returning a 0. Otherwise the column to the right
+        // is just the column + 1.
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+
+        // We calculate the index of all neighboring cells and add their
+        // value to our count variable. We can pass the direction as the
+        // row and column to the get_index method, because they're
+        // concrete identifiers for which columns and rows are adjacent to
+        // our cell of interest.
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
+
         count
     }
 }
@@ -169,58 +244,65 @@ impl Universe {
     pub fn tick(&mut self) {
         // Create a new Timer that tracks each tick of the universe.
         // Due to the nature of the tick method, a new timer is created
-        // for each tick.
+        // for each tick. When the timer is dropped, the time it took
+        // to execute the tick method is printed to the console. The timer
+        // is dropped at the end of the tick method, when it goes out of
+        // scope.
         let _timer = utils::Timer::new("Universe::tick");
         // Clone the current cells into a new vector:
         let mut next = self.cells.clone();
+        let _timer = utils::Timer::new("allocate new cells");
 
-        for row in 0..self.height {
-            for col in 0..self.width {
-                // Get the index of the current cell:
-                let idx = self.get_index(row, col);
-                // Get the cell at the specific index:
-                let cell = self.cells[idx];
-                // Count the number of living neighbors:
-                let live_neighbors = self.live_neighbor_count(row, col);
+        {
+            let _timer = utils::Timer::new("new generation");
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    // Get the index of the current cell:
+                    let idx = self.get_index(row, col);
+                    // Get the cell at the specific index:
+                    let cell = self.cells[idx];
+                    // Count the number of living neighbors:
+                    let live_neighbors = self.live_neighbor_count(row, col);
 
-                // Log the amount of living cells and initial state to console output:
-                // log!(
-                //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                //     row,
-                //     col,
-                //     cell,
-                //     live_neighbors
-                // );
+                    // Log the amount of living cells and initial state to console output:
+                    // log!(
+                    //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                    //     row,
+                    //     col,
+                    //     cell,
+                    //     live_neighbors
+                    // );
 
-                // Determine the state of the cell in the next tick in time:
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
+                    // Determine the state of the cell in the next tick in time:
+                    let next_cell = match (cell, live_neighbors) {
+                        // Rule 1: Any live cell with fewer than two live neighbours
+                        // dies, as if caused by underpopulation.
+                        (Cell::Alive, x) if x < 2 => Cell::Dead,
+                        // Rule 2: Any live cell with two or three live neighbours
+                        // lives on to the next generation.
+                        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                        // Rule 3: Any live cell with more than three live
+                        // neighbours dies, as if by overpopulation.
+                        (Cell::Alive, x) if x > 3 => Cell::Dead,
+                        // Rule 4: Any dead cell with exactly three live neighbours
+                        // becomes a live cell, as if by reproduction.
+                        (Cell::Dead, 3) => Cell::Alive,
+                        // All other cells remain in the same state.
+                        (otherwise, _) => otherwise,
+                    };
 
-                // Log state change of each cell to console output:
-                // log!("It becomes {:?}", next_cell);
+                    // Log state change of each cell to console output:
+                    // log!("It becomes {:?}", next_cell);
 
-                // Insert the `next_cell` into the array of
-                // cells at the next tick in time:
-                next[idx] = next_cell;
+                    // Insert the `next_cell` into the array of
+                    // cells at the next tick in time:
+                    next[idx] = next_cell;
+                }
             }
         }
-
         // Overwrite the current array of cells with
         // those at the current time plus one tick:
+        let _timer = utils::Timer::new("free old cells");
         self.cells = next;
     }
 
@@ -238,8 +320,8 @@ impl Universe {
         utils::set_panic_hook();
 
         let universe_option = universe_option;
-        let width = 512;
-        let height = 512;
+        let width = 256;
+        let height = 256;
 
         /// Returns a vector of random `Cell` instances.
         ///
