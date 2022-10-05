bd722429cbcfcee899f528c95b71a0a0c3b2742f
tobiplay <tobiplay@fa-meier.de>
2022-10-05 14:11:53 +0300

    Switch to `adapter-auto`

---
+++ svelte/.netlify/functions-internal/render.mjs
@@ -0,0 +1,29 @@
+import { init } from '../serverless.js';
+
+export const handler = init({
+	appDir: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"_app/immutable/start-f6b0f064.js","imports":["_app/immutable/start-f6b0f064.js","_app/immutable/chunks/index-9bcca362.js","_app/immutable/chunks/singletons-37d03af2.js"],"stylesheets":[]},
+		nodes: [
+			() => import('../server/nodes/0.js'),
+			() => import('../server/nodes/1.js'),
+			() => import('../server/nodes/2.js')
+		],
+		routes: [
+			{
+				id: "",
+				pattern: /^\/$/,
+				names: [],
+				types: [],
+				page: { layouts: [0], errors: [1], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
