7e3fdd962179e38814db4ef73ff3fc0d8f546714
tobiplay <tobiplay@fa-meier.de>
2022-10-06 00:29:50 +0300

    Remove `/` from file path

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -5,16 +5,16 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), wasm(), topLevelAwait()],
-	server: {
-		fs: {
-			allow: [
-				// We have to search the workspace root because the Vite config
-				// is in a subdirectory of the workspace root:
-				searchForWorkspaceRoot(process.cwd()),
-				// Our custom path to be allowed by the Vite server:
-				'../rust/pkg'
-			]
-		}
-	}
+  plugins: [sveltekit(), wasm(), topLevelAwait()],
+  server: {
+    fs: {
+      allow: [
+        // We have to search the workspace root because the Vite config
+        // is in a subdirectory of the workspace root:
+        searchForWorkspaceRoot(process.cwd()),
+        // Our custom path to be allowed by the Vite server:
+        'pkg'
+      ]
+    }
+  }
 });
